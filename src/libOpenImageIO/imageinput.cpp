2b331b3e96e4d54cf31fa09db168a15cfe33d508
Larry Gritz <lg@larrygritz.com>
2024-04-15 19:25:57 -0700

    WIP try re-enabling the input error message erasing
    Signed-off-by: Larry Gritz <lg@larrygritz.com>

--- src/libOpenImageIO/imageinput.cpp
+++ src/libOpenImageIO/imageinput.cpp
@@ -86,7 +86,7 @@ ImageInput::~ImageInput()
     // Erase any leftover errors from this thread
     // TODO: can we clear other threads' errors?
     // TODO: potentially unsafe due to the static destruction order fiasco
-    // input_error_messages.erase(this);
+    input_error_messages.erase(this);
 }
 
 
