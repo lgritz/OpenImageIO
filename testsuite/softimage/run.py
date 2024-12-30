d408d53c1c2553feee7911a0b86837c8439f2813
Larry Gritz <lg@larrygritz.com>
2024-12-30 10:06:37 -0800

    Update softimage test

--- testsuite/softimage/run.py
+++ testsuite/softimage/run.py
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # https://github.com/AcademySoftwareFoundation/OpenImageIO
 
-files = [ "fisheye.pic", "astone64.pic" ]
+files = [ "A4.pic", "astone64.pic" ]
 for f in files:
     command += info_command (OIIO_TESTSUITE_IMAGEDIR + "/" + f, extraargs="--stats")
 
