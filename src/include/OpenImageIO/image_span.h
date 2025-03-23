6f16bb644e4c687abe28d51ba27fa1f46aabf873
Larry Gritz <lg@larrygritz.com>
2025-03-22 22:19:12 -0700

    wip
    Signed-off-by: Larry Gritz <lg@larrygritz.com>

--- src/include/OpenImageIO/image_span.h
+++ src/include/OpenImageIO/image_span.h
@@ -33,190 +33,14 @@ inline constexpr stride_t AutoStride = std::numeric_limits<stride_t>::min();
 
 
 
-namespace detail {
-
-// template<typename T, size_t Dims>
-// struct dim {
-//     dim()
-// private:
-//     std::array<T, Dims> m_vals;
-// };
-
-#if 0
-using stride_t = int64_t;
-static constexpr stride_t AutoStride = std::numeric_limits<stride_t>::min();
-
-
-template<typename T, int Dims> class image_span_storage {
-    static_assert(0 && Dims >= 2 && Dims <= 4, "Dimension must be between 2 and 4");
-};
-
-
-template<typename T> class image_span_storage<T, 4> {
-public:
-    image_span_storage(T* data, uint32_t nchannels, uint32_t width,
-                       uint32_t height, uint32_t depth, int64_t chanstride,
-                       int64_t xstride, int64_t ystride, int64_t zstride,
-                       uint32_t chansize = sizeof(T))
-        : m_data(data)
-        , m_nchannels(nchannels)
-        , m_width(width)
-        , m_height(height)
-        , m_depth(depth)
-        , m_chanstride(chanstride != AutoStride ? chanstride : chansize)
-        , m_xstride(xstride != AutoStride ? xstride
-                                          : m_nchannels * m_chanstride)
-        , m_ystride(ystride != AutoStride ? ystride : m_width * m_xstride)
-        , m_zstride(zstride != AutoStride ? zstride : m_height * m_ystride)
-        , m_chansize(chansize)
-    {
-    }
-
-public:
-    T* m_data;
-    uint32_t m_nchannels, m_width, m_height, m_depth;
-    int64_t m_chanstride, m_xstride, m_ystride, m_zstride;
-    uint32_t m_chansize { sizeof(T) };
-};
-
-
-template<typename T> struct image_span_storage<T, 3> {
-protected:
-    T* m_data;
-    uint32_t m_nchannels, m_width, m_height;
-    int64_t m_chanstride, m_xstride, m_ystride;
-    uint32_t m_chansize { sizeof(T) };
-};
-
-
-template<typename T> struct image_span_storage<T, 2> {
-protected:
-    T* m_data;
-    uint32_t m_nchannels, m_width;
-    int64_t m_chanstride, m_xstride;
-    uint32_t m_chansize { sizeof(T) };
-};
-#endif
-
-
-#if 0
-template<size_t Dims = 4> struct image_span_bounds_storage {
-    static_assert(Dims >= 2 && Dims <= 4, "Dimension must be between 2 and 4");
-    static_assert(sizeof(image_span_bounds_storage)
-                      == Dims * 2 * sizeof(size_t),
-                  "Unexpected size of image_span_bounds_storage!");
-
-    using stride_type = int64_t;
-    using size_type = uint32_t;
-
-    std::array<size_type, Dims> m_sizes;
-    std::array<stride_type, Dims> m_strides;
-
-    constexpr size_type nchannels() const { return m_sizes[0]; }
-    constexpr stride_type chanstride() const { return m_strides[0]; }
-
-    constexpr size_type width() const { return m_sizes[1]; }
-    constexpr stride_type xstride() const { return m_strides[1]; }
-
-    constexpr size_type height() const
-    {
-        if constexpr (Dims >= 2)
-            return m_sizes[2];
-        else
-            return 1;
-    }
-    constexpr stride_type ystride() const
-    {
-        if constexpr (Dims >= 2)
-            return m_strides[2];
-        else
-            return 0;
-    }
-
-    constexpr size_type depth() const
-    {
-        if constexpr (Dims >= 3)
-            return m_sizes[3];
-        else
-            return 1;
-    }
-    constexpr stride_type zstride() const
-    {
-        if constexpr (Dims >= 3)
-            return m_strides[3];
-        else
-            return 0;
-    }
-};
-
-
-
-template<size_t Dims = 4> struct image_span_bounds_storage2 {
-    static_assert(Dims >= 2 && Dims <= 4, "Dimension must be between 2 and 4");
-    static_assert(sizeof(image_span_bounds_storage2)
-                      == Dims * 2 * sizeof(size_t),
-                  "Unexpected size of image_span_bounds_storage!");
-
-    using stride_type = int64_t;
-    using size_type = uint32_t;
-
-    std::array<size_type, Dims> m_sizes;
-    std::array<stride_type, Dims> m_strides;
-
-    constexpr size_type nchannels() const { return m_sizes[0]; }
-    constexpr stride_type chanstride() const { return m_strides[0]; }
-
-    constexpr size_type width() const { return m_sizes[1]; }
-    constexpr stride_type xstride() const { return m_strides[1]; }
-
-    template<size_t D = Dims>
-    constexpr size_type height() const
-    {
-        if constexpr (Dims >= 2)
-            return m_sizes[2];
-        else
-            return 1;
-    }
-
-    constexpr stride_type ystride() const
-    {
-        if constexpr (Dims >= 2)
-            return m_strides[2];
-        else
-            return 0;
-    }
-
-    constexpr size_type depth() const
-    {
-        if constexpr (Dims >= 3)
-            return m_sizes[3];
-        else
-            return 1;
-    }
-    constexpr stride_type zstride() const
-    {
-        if constexpr (Dims >= 3)
-            return m_strides[3];
-        else
-            return 0;
-    }
-};
-#endif
-
-}
-
-
-
-/// image_span : a non-owning reference to a 4D image-like array (having
-/// channel, x, y, z) with known dimensions and optionally non-default strides
-/// (expressed in bytes) through the data.  An image_span<T> is mutable (the
-/// values in the image may be modified), whereas an image_span<const T> is
-/// not mutable.
+/// image_span : a non-owning reference to n-D image-like array having between
+/// 2 and 4 dimensions representing channel, x, y, z with each dimension
+/// having known size and optionally non-default strides (expressed in bytes)
+/// through the data.  An image_span<T> is mutable (the values in the image
+/// may be modified), whereas an image_span<const T> is not mutable.
 ///
-/// Another way to describe an image_span is as a 4-dimensional span with
-/// byte strides.
-template<typename T, int Dims = 4>
-class image_span /*: public detail::image_span_storage<T, Dims>*/ {
+template<typename T, size_t Dims = 4>
+class image_span {
     static_assert(Dims >= 2 && Dims <= 4, "Dimension must be between 2 and 4");
 
 public:
@@ -241,21 +65,8 @@ public:
                uint32_t depth = 1, stride_t chanstride = AutoStride,
                stride_t xstride = AutoStride, stride_t ystride = AutoStride,
                stride_t zstride = AutoStride, uint32_t chansize = sizeof(T))
-        // : detail::image_span_storage<T, Dims>(data, nchannels, width, height,
-        //                                       depth, chanstride, xstride,
-        //                                       ystride, zstride, chansize)
         : m_data(data)
         , m_sizes({ nchannels, width, height, depth })
-        // , m_strides({ chanstride, xstride, ystride, zstride })
-        // , m_nchannels(nchannels)
-        // , m_width(width)
-        // , m_height(height)
-        // , m_depth(depth)
-        // , m_chanstride(chanstride != AutoStride ? chanstride : chansize)
-        // , m_xstride(xstride != AutoStride ? xstride
-        //                                   : m_nchannels * m_chanstride)
-        // , m_ystride(ystride != AutoStride ? ystride : m_width * m_xstride)
-        // , m_zstride(zstride != AutoStride ? zstride : m_height * m_ystride)
         , m_chansize(chansize)
     {
         chanstride = chanstride != AutoStride ? chanstride : chansize;
@@ -309,6 +120,8 @@ public:
     //     return (*this)(uint32_t(x), uint32_t(y), uint32_t(x));
     // }
 #endif
+
+    static constexpr size_t rank() noexcept { return Dims; }
 
     constexpr size_type nchannels() const { return m_sizes[0]; }
     constexpr stride_type chanstride() const { return m_strides[0]; }
@@ -430,17 +243,7 @@ private:
     T* m_data { nullptr };
     std::array<stride_type, Dims> m_strides;
     std::array<size_type, Dims> m_sizes;
-#if 0
-    uint32_t m_nchannels { 0 }, m_width { 0 }, m_height { 0 }, m_depth { 0 };
-    stride_t m_chanstride { 0 }, m_xstride { 0 }, m_ystride { 0 },
-        m_zstride { 0 };
-#endif
     uint32_t m_chansize { sizeof(T) };
-    // bool m_contiguous { true };
-
-    // static_assert(sizeof(image_span)
-    //                   == Dims * sizeof(size_t),
-    //               "Unexpected size of image_span!");
 };
 
 
