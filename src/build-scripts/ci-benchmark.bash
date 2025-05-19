30c4bae3541d1a4d73451907d186dab7a748d3db
Larry Gritz <lg@larrygritz.com>
2025-05-19 09:54:02 -0700

    benchmark ARM, add simd_test to benchmark
    Signed-off-by: Larry Gritz <lg@larrygritz.com>

--- src/build-scripts/ci-benchmark.bash
+++ src/build-scripts/ci-benchmark.bash
@@ -13,12 +13,12 @@ ls build
 ls $BUILD_BIN_DIR
 
 mkdir -p build/benchmarks
-for t in image_span_test ; do
+for t in image_span_test simd_test ; do
     echo
     echo
     echo "$t"
     echo "========================================================"
-    ${BUILD_BIN_DIR}/$t > build/benchmarks/$t.out
+    time ${BUILD_BIN_DIR}/$t > build/benchmarks/$t.out
     cat build/benchmarks/$t.out
     echo "========================================================"
     echo "========================================================"
