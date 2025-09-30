d44b692201c1f0016cf3bcba9126378d3a1af2ff
Larry Gritz <lg@larrygritz.com>
2025-09-30 08:55:57 -0700

    Build ccache from source
    Signed-off-by: Larry Gritz <lg@larrygritz.com>

--- src/build-scripts/build_ccache.bash
+++ src/build-scripts/build_ccache.bash
@@ -11,19 +11,52 @@ set -ex
 
 echo "Building ccache"
 uname
+echo "HOME=$HOME"
 
+# Repo and branch/tag/commit of ccache to download if we don't have it yet
+CCACHE_REPO=${CCACHE_REPO:=https://github.com/ccache/ccache}
 CCACHE_VERSION=${CCACHE_VERSION:=4.12}
 CCACHE_TAG=${CCACHE_TAG:=v${CCACHE_VERSION}}
 LOCAL_DEPS_DIR=${LOCAL_DEPS_DIR:=${PWD}/ext}
-CCACHE_INSTALL_DIR=${CCACHE_INSTALL_DIR:=${LOCAL_DEPS_DIR}/ccache}
+# Where to put ccache repo source (default to the ext area)
+CCACHE_SRC_DIR=${CCACHE_SRC_DIR:=${LOCAL_DEPS_DIR}/ccache}
+# Temp build area (default to a build/ subdir under source)
+CCACHE_BUILD_DIR=${CCACHE_BUILD_DIR:=${CCACHE_SRC_DIR}/build}
+# Install area for ccache (default to ext/dist)
+CCACHE_INSTALL_DIR=${CCACHE_INSTALL_DIR:=${PWD}/ext/dist}
+CCACHE_CONFIG_OPTS=${CCACHE_CONFIG_OPTS:=}
+
 
 if [[ `uname` == "Linux" && `uname -m` == "x86_64" ]] ; then
-    mkdir -p ${CCACHE_INSTALL_DIR} || true
-    mkdir -p ${LOCAL_DEPS_DIR}/bin || true
-    pushd ${CCACHE_INSTALL_DIR}
-    CCACHE_DESCRIPTOR="ccache-${CCACHE_VERSION}-linux-x86_64"
-    curl --location "https://github.com/ccache/ccache/releases/download/${CCACHE_TAG}/${CCACHE_DESCRIPTOR}.tar.xz" -o ccache.tar.xz
-    tar xJvf ccache.tar.xz
-    cp ${CCACHE_INSTALL_DIR}/${CCACHE_DESCRIPTOR}/ccache ${LOCAL_DEPS_DIR}/bin
+    mkdir -p ${CCACHE_SRC_DIR}
+    pushd ${CCACHE_SRC_DIR}
+
+    #
+    # Try to download -- had trouble with this on runners
+    #
+    # mkdir -p ${LOCAL_DEPS_DIR}/bin
+    # CCACHE_DESCRIPTOR="ccache-${CCACHE_VERSION}-linux-x86_64"
+    # curl --location "${CCACHE_REPO}/releases/download/${CCACHE_TAG}/${CCACHE_DESCRIPTOR}.tar.xz" -o ccache.tar.xz
+    # tar xJvf ccache.tar.xz
+    # cp ${CCACHE_SRC_DIR}/${CCACHE_DESCRIPTOR}/ccache ${CCACHE_INSTALL_DIR}/bin
+
+    # Clone ccache project from GitHub and build
+    if [[ ! -e ${CCACHE_SRC_DIR}/.git ]] ; then
+        echo "git clone ${CCACHE_REPO} ${CCACHE_SRC_DIR}"
+        git clone ${CCACHE_REPO} ${CCACHE_SRC_DIR}
+    fi
+
+    echo "git checkout ${CCACHE_TAG} --force"
+    git checkout ${CCACHE_TAG} --force
+
+    if [[ -z $DEP_DOWNLOAD_ONLY ]]; then
+        time cmake -S . -B ${CCACHE_BUILD_DIR} -DCMAKE_BUILD_TYPE=Release \
+                   -DCMAKE_INSTALL_PREFIX=${CCACHE_INSTALL_DIR} \
+                   ${CCACHE_CONFIG_OPTS}
+        time cmake --build ${CCACHE_BUILD_DIR} --config Release --target install
+    fi
+
     popd
+    ls ${CCACHE_INSTALL_DIR}
+    ls ${CCACHE_INSTALL_DIR}/bin
 fi
