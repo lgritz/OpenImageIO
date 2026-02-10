78ae88b554917b928fe03ed41994e046863b6455
Larry Gritz <lg@larrygritz.com>
2026-02-10 00:19:41 -0800

    MSVS needs /utf-8 compiler option for fmt headers, so make it part of OpenImageIO_Util's interface compile options
    Signed-off-by: Larry Gritz <lg@larrygritz.com>

--- src/cmake/build_fmt.cmake
+++ src/cmake/build_fmt.cmake
@@ -22,8 +22,6 @@ build_dependency_with_cmake(fmt
         -D FMT_TEST=OFF
     )
 
-# Set some things up that we'll need for a subsequent find_package to work
-set (fmt_ROOT ${fmt_INSTALL_DIR})
-
 # Signal to caller that we need to find again at the installed location
 set (fmt_REFIND TRUE)
+set (fmt_VERSION ${fmt_BUILD_VERSION})
